62241713f0179c879b4e1e5ec31dbd84d56eaae1
Nathan Vance <nathan@vance.homelinuxserver.org>
2018-10-03 15:25:14 -0400

    Stripped down code

--- demo/client.c
+++ demo/client.c
@@ -1,94 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
-#include <sys/time.h>
-#include <limits.h>
-#include <dirent.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 #include "sockets.h"
 
 void clientLoop(int sock) {
-    char buffer[BUFFER];
     char input[2048];
     printf("> ");
     while(fgets(input, 2048, stdin)) {
-        // Remove newline
-        input[strlen(input)-1] = '\0';
-        // Handle input
-        if(strcmp(input, "LS") == 0) {
-            snd(sock, input);
-            printf("%s", rcv(sock, buffer));
-        } else if(strcmp(input, "EXIT") == 0) {
-            snd(sock, input);
-            exit(0);
-        } else if(strstr(input, "CD ") == input) {
-            snd(sock, input);
-            int ret = rcvInt(sock);
-            if(ret == -2) printf("The directory does not exist on server\n");
-            else if(ret == -1) printf("Error in changing directory\n");
-            else printf("Changed current directory\n");
-        } else if(strstr(input, "DL ") == input) {
-            snd(sock, input);
-            if(! rcvInt(sock)) {
-	        char *md5 = md5sum(input+3);
-		char *in = (strrchr(input, '/') == NULL)? input+3 : strrchr(input, '/')+1;
-		rcvFile(sock, in);
-	        snd(sock, md5);
-	        free(md5);
-                printf("%s", rcv(sock, buffer));
-	    }
-            else printf("File does not exist\n");
-        } else if(strstr(input, "UP ") == input) {
-            snd(sock, input);
-	    char *md5 = md5sum(input+3);
-	    //printf("Calculated: %s\n", md5);
-	    snd(sock, md5);
-	    free(md5);
-            sndFile(sock, input+3);
-            printf("%s", rcv(sock, buffer));
-        } else if(strstr(input, "RM ") == input) {
-            snd(sock, input);
-            if(rcvInt(sock)) {
-                printf("ERROR: File does not exist\n> ");
-                continue;
-            }
-            printf("Are you sure? ");
-            fgets(input, 2048, stdin);
-            input[strlen(input)-1] = '\0';
-            snd(sock, input);
-            if(strcmp(input, "Yes") != 0) {
-                printf("Delete abandoned by the user!\n");
-            } else {
-                if(rcvInt(sock)) printf("ERROR: Incorrect permissions\n");
-                else printf("File deleted.\n");
-            }
-        } else if(strstr(input, "MKDIR ") == input) {
-            snd(sock, input);
-            int retval = rcvInt(sock);
-            if(retval == -2) printf("The directory already exists on server\n");
-            else if(retval == -1) printf("Error in making directory\n");
-            else printf("The directory was successfully made\n");
-        } else if(strstr(input, "RMDIR ") == input) {
-            snd(sock, input);
-            int retval = rcvInt(sock);
-            if(retval == -2) printf("The directory is not empty\n");
-            else if(retval == -1) printf("The directory does not exist on server\n");
-            else {
-                printf("Are you sure? ");
-                fgets(input, 2048, stdin);
-                input[strlen(input)-1] = '\0';
-                snd(sock, input);
-		if(strcmp(input, "Yes") != 0) {
-                    printf("Delete abandoned by the user!\n");
-                } else {
-                    if(rcvInt(sock)) printf("Failed to delete directory\n");
-                    else printf("Directory deleted.\n");
-                }
-            }
-        }
+        snd(sock, input);
         printf("> ");
     }
 }
@@ -102,7 +25,23 @@ int main(int argc, char *argv[]) {
     if(argc >= 3)
             port = atoi(argv[2]);
     printf("Connecting to %s on port %d\n", host, port);
-    int sock = makeConn(host, port);
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+    struct hostent *he;
+    if((he = gethostbyname(host)) != NULL) {
+        struct in_addr *address = (struct in_addr*) he->h_addr_list[0];
+        host = (char*) calloc(64, sizeof(char));
+        strcpy(host, inet_ntoa(*address));
+    }
+    inet_pton(AF_INET, host, &addr.sin_addr);
+    if(connect(sock, (struct sockaddr*) &addr, sizeof(addr)) != 0) {
+        printf("Connection failed\n");
+	exit(1);
+    }
     printf("Connection established\n");
     clientLoop(sock);
     return 0;
--- demo/server.c
+++ demo/server.c
@@ -11,110 +11,16 @@
 #include <pthread.h>
 #include "sockets.h"
 
-void xfer(int sock, int isRcv, char *fname) {
-    struct timeval tv;
-    gettimeofday(&tv,NULL);
-    unsigned long before = 1000000 * tv.tv_sec + tv.tv_usec;
-    unsigned long size;
-    char *md5source, *md5dest;
-    char buffer[BUFFER];
-    if(isRcv) {
-	char *tmp = rcv(sock, buffer);
-	md5source = malloc(sizeof(char) * strlen(tmp));
-	strcpy(md5source, tmp);
-        size = rcvFile(sock, fname);
-	md5dest = md5sum(fname);
-    } else {
-        md5source = md5sum(fname);
-        size = sndFile(sock, fname);
-	md5dest = rcv(sock, buffer);
-    }
-    gettimeofday(&tv,NULL);
-    unsigned long duration = 1000000 * tv.tv_sec + tv.tv_usec - before;
-    unsigned long rate = size * 1000000LU / duration;
-    float mbps = (float) rate / 1000000;
-    float secs = (float) duration / 1000000;
-    char throughput[1024];
-    //printf("md5 source/dest:\n\t\"%s\"\n\t\"%s\"\n", md5source, md5dest);
-    sprintf(throughput, "%lu bytes transferred in %f seconds: %f Megabytes/sec\n\tMD5 hash: %s %s\n", size, secs, mbps, md5dest, (strcmp(md5source, md5dest) == 0)? "(matches)" : "ERROR: Does not match!");
-    snd(sock, throughput);
-}
-
-//void* serveLoop(void *s) {
-//    int sock = *((int*) s);
-int serveLoop(int sock) {
-    char buffer[BUFFER];
+void* serveLoop(void *s) {
+    int sock = *((int*) s);
+//int serveLoop(int sock) {
+    char buffer[2048];
     while(1) {
-        char *op = rcv(sock, buffer);
-	//printf("RCVD: %s\n", op);
-        if(strcmp(op, "LS") == 0) {
-            char *ls = sysc("ls -l");
-	    snd(sock, ls);
-	    free(ls);
-        }
-	else if(strcmp(op, "EXIT") == 0) {
-	    cleanup(sock);
-	    return 0;
+        if(rcv(sock, buffer) < 1) {
+	    close(sock);
+            return 0;
 	}
-	else if(strstr(op, "CD ") == op) {
-	    int res = chdir(op+3);
-	    if(errno == ENOTDIR) sndInt(sock, -2);
-	    else sndInt(sock, res);
-	}
-	else if(strstr(op, "DL ") == op) {
-	    if(!access(op+3, F_OK)) {
-		sndInt(sock, 0);
-		char fname[strlen(op)];
-		strcpy(fname, op+3);
-		xfer(sock, 0, fname);
-	    } else {
-                sndInt(sock, -1);
-	    }
-	}
-	else if(strstr(op, "UP ") == op) {
-	    op = (strrchr(op, '/') == NULL)? op+3 : strrchr(op, '/')+1;
-            char fname[strlen(op)];
-	    strcpy(fname, op);
-	    xfer(sock, 1, fname);
-	} else if(strstr(op, "RM ") == op) {
-            char fname[strlen(op)];
-	    strcpy(fname, op+3);
-	    if(access(fname, F_OK)) {
-	        sndInt(sock, -1);
-		continue;
-	    }
-	    sndInt(sock, 0);
-	    if(strcmp(rcv(sock, buffer), "Yes") == 0) {
-	        sndInt(sock, remove(fname));
-	    }
-	} else if(strstr(op, "MKDIR ") == op) {
-	    DIR *d = opendir(op+6);
-	    if(d) {
-                sndInt(sock, -2);
-		closedir(d);
-	    } else {
-                sndInt(sock, mkdir(op+6, 0700));
-	    }
-	} else if(strstr(op, "RMDIR ") == op) {
-            char dname[strlen(op)];
-            strcpy(dname, op+6);
-	    DIR *d = opendir(dname);
-	    if(d == NULL) {
-                sndInt(sock, -1);
-		continue;
-	    }
-	    int n;
-	    for(n=0; n <= 2 && readdir(d) != NULL; n++);
-	    closedir(d);
-            if(n == 3) {
-                sndInt(sock, -2);
-                continue;
-            }
-            sndInt(sock, 0);
-            if(strcmp(rcv(sock, buffer), "Yes") == 0) {
-                sndInt(sock, rmdir(dname));
-            }
-	}
+	printf(buffer);
     }
 }
 
@@ -132,13 +38,13 @@ int main(int argc, char *argv[]) {
     addr.sin_port = htons(port);
     bind(serversock, (struct sockaddr*) &addr, sizeof(addr));
     listen(serversock, 1);
+    printf("Accepting connections on port %d\n", port);
     while(1) {
-        printf("Accepting connections on port %d\n", port);
         int sock = accept(serversock, NULL, NULL);
         printf("Connection established\n");
-	//pthread_t pth;
-	//pthread_create(&pth, NULL, serveLoop, &sock);
-        serveLoop(sock);
+	pthread_t pth;
+	pthread_create(&pth, NULL, serveLoop, &sock);
+        //serveLoop(sock);
     }
     return 0;
 }
